ee82721be03554a95c1837caffbb07e73858d3f3
Szymon Kuchnia <szymon.kuchnia@dpksystem.pl>
2016-11-29 05:10:28 +0100

    step_3

--- Activity1Example/app/src/main/java/com/example/agh/activity1example/MainActivity.java
+++ Activity1Example/app/src/main/java/com/example/agh/activity1example/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.agh.activity1example;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("agh.activity1example", "uruchomiono onCreate()");
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("agh.activity1example", "uruchomiono onPause()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("agh.activity1example", "uruchomiono onStart()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("agh.activity1example", "uruchomiono onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("agh.activity1example", "uruchomiono onResume()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("agh.activity1example", "uruchomiono onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("agh.activity1example", "uruchomiono onStop()");
+    }
+
 }
